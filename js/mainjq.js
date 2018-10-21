3446208a796623db2e5bc4b8639d4e7778fd06bb
FunMan22 <43394783+FunMan22@users.noreply.github.com>
2018-10-21 16:12:57 -0400

    Update mainjq.js

--- js/mainjq.js
+++ js/mainjq.js
@@ -33,45 +33,45 @@ $(document).ready(function() {
       "padding-left": paddingwidth,
       "padding-right": paddingwidth
    });
-
-
-
-   function checkScroll() {
-      var column = document.getElementById("columnleft");
-      var sticky = column.offsetTop - 46;
-      var clientHeight = $("#banner").height();
-      if (window.pageYOffset >= sticky) {
-         $("#columnleft").addClass("sticky");
-         $("#columnright").addClass("sticky");
-         $("#navbar").addClass("navshadowon");
-         $("#navbar").removeClass("navshadowoff");
-      }
-      if (window.pageYOffset <= clientHeight) {
-         $("#columnleft").removeClass("sticky");
-         $("#columnright").removeClass("sticky");
-         $("#navbar").removeClass("navshadowon");
-         if (timesRun !== 0) {
-            $("#navbar").addClass("navshadowoff");
-         }
-      }
-      timesRun += 1;
-
-   }
-   
-   
-   function checkResize() {
-      // Gets availible width in the nav bar
-      var takenWidth = $("h2.title-text").width();
-      var totalWidth = $(window).width();
-      var remainWidth = totalWidth - takenWidth;
-      // makes the ul the size of the remaining width on the page. (Acts as a container)
-      $("ul.nav-list").css({
-         "width": remainWidth + "px"
-      });
-   }
-
-
-
-
-
 });
+
+
+function checkScroll() {
+   var column = document.getElementById("columnleft");
+   var sticky = column.offsetTop - 46;
+   var clientHeight = $("#banner").height();
+   if (window.pageYOffset >= sticky) {
+      $("#columnleft").addClass("sticky");
+      $("#columnright").addClass("sticky");
+      $("#navbar").addClass("navshadowon");
+      $("#navbar").removeClass("navshadowoff");
+   }
+   if (window.pageYOffset <= clientHeight) {
+      $("#columnleft").removeClass("sticky");
+      $("#columnright").removeClass("sticky");
+      $("#navbar").removeClass("navshadowon");
+      if (timesRun !== 0) {
+         $("#navbar").addClass("navshadowoff");
+      }
+   }
+   timesRun += 1;
+
+}
+
+
+function checkResize() {
+   // Gets availible width in the nav bar
+   var takenWidth = $("h2.title-text").width();
+   var totalWidth = $(window).width();
+   var remainWidth = totalWidth - takenWidth;
+   // makes the ul the size of the remaining width on the page. (Acts as a container)
+   $("ul.nav-list").css({
+      "width": remainWidth + "px"
+   });
+}
+
+
+
+
+
+
